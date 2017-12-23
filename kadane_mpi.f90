7a4a45cf7fe77af8dea9687e8126d96b24ba20fe
Marina Sosnova <svet001@bk.ru>
2017-12-24 02:27:57 +0300

    Новая MPI версия

--- kadane_mpi.f90
+++ kadane_mpi.f90
@@ -1,87 +1,47 @@
-module kadane_mpi
-	implicit none
-	contains
-		subroutine kadane
-			use mpi
-!			include "mpif.h"
-			integer(4) :: maximum, t, r1, c1, r2, c2, g, N, M, h, i, j
-			integer(4), allocatable :: a (:,:)  !integer(4), dimension(:,:), allocatable :: a
-			integer(4), allocatable :: p (:)    !integer(4), dimension(:  ), allocatable :: p
-			real :: r
-			integer(4) :: mpi_master, mpi_rank, mpi_size, mpi_err
-			integer(4), dimension(2) :: Ms, Mso
-			INTEGER :: status(MPI_STATUS_SIZE)
-			call mpi_init(mpi_err)
-			mpi_master = 0
-			call mpi_comm_rank(mpi_comm_world, mpi_rank, mpi_err)
-			call mpi_comm_size(mpi_comm_world, mpi_size, mpi_err)
-			if (mpi_rank == mpi_master) then
-				write(*,*) "N, M = ?" 
-				read(*,*) N, M
-			end if
-			call mpi_bcast(N, 1, mpi_integer, mpi_master, mpi_comm_world, mpi_err)
-			call mpi_barrier(mpi_comm_world, mpi_err)
-			call mpi_bcast(M, 1, mpi_integer, mpi_master, mpi_comm_world, mpi_err)
-			call mpi_barrier(mpi_comm_world, mpi_err)
-			allocate(a(N,M))
-			allocate(p(M))
-			if (mpi_rank == mpi_master) then
-				call random_seed
-				do i = 1, N
-					do j = 1, M
-						call random_number(r)
-						a(i,j) = floor(r * 100-50) 
-						write (*,"(I5)", advance="no") a(i,j)
-					end do
-					write (*,*)
-				end do
-			end if
-			call mpi_bcast(a, N*M, mpi_integer, mpi_master, mpi_comm_world, mpi_err)
-			call mpi_barrier(mpi_comm_world, mpi_err)
+module Homework_MPI
+  implicit none
+  contains
+    subroutine FindMaxCoordinates(A, x1, y1, x2, y2)
+      use mpi
+      real(8), allocatable, intent(in) :: A (:,:)
+      integer(4), intent(out) :: x1, y1, x2, y2
+      real(8), allocatable :: p (:), maximums (:)
+      integer(4) :: h, g, i, j, N, M
+      real(8) :: maximum, t
+      integer(4) :: mpi_master, mpi_rank, mpi_size, mpi_err, mpi_correct
 
-			maximum = 0; r1  = 0; c1  = 0; r2  = 0; c2  = 0
-			do g = 1, N
-				if (mod(g, mpi_size) == mpi_rank) then
-					do j = 1, M
-						p(j) = 0
-					end do
-					do i = g, N
-						t = 0; h = 1
-						do j = 1 , M
-							p(j) = p(j) + a(i,j); t = t+p(j)
-							if (t>maximum) then
-								maximum=t; r1=g; c1=h; r2=i; c2=j
-							else if (t<=0) then
-								t=0; h =j+1
-							end if
-						end do
-					end do
-				end if
-			end do
-			Ms(1) = maximum; Ms(2) = mpi_rank
-			call mpi_barrier(mpi_comm_world, mpi_err)
-			call mpi_allreduce(Ms,Mso,1,mpi_2integer,mpi_maxloc,mpi_comm_world,mpi_err)
-			call mpi_barrier(mpi_comm_world, mpi_err)
-!		 	write (*,*) "#", mpi_rank, M, r1, c1, r2, c2
-			if(Mso(2) /= mpi_master) then
-				if(Mso(2) == mpi_rank) then
-					call mpi_send(r1, 1, mpi_integer, mpi_master, 0, mpi_comm_world, mpi_err)
-					call mpi_send(r2, 1, mpi_integer, mpi_master, 1, mpi_comm_world, mpi_err)
-					call mpi_send(c1, 1, mpi_integer, mpi_master, 2, mpi_comm_world, mpi_err)
-					call mpi_send(c2, 1, mpi_integer, mpi_master, 3, mpi_comm_world, mpi_err)
-				else if(mpi_rank == mpi_master) then
-					maximum = Mso(1)
-					call mpi_recv(r1, 1, mpi_integer, Mso(2), 0, mpi_comm_world, status, mpi_err)
-					call mpi_recv(r2, 1, mpi_integer, Mso(2), 1, mpi_comm_world, status, mpi_err)
-					call mpi_recv(c1, 1, mpi_integer, Mso(2), 2, mpi_comm_world, status, mpi_err)
-					call mpi_recv(c2, 1, mpi_integer, Mso(2), 3, mpi_comm_world, status, mpi_err)
-				end if
-			end if
-			call mpi_barrier(mpi_comm_world, mpi_err)
-			if(mpi_rank == mpi_master) then
-				write (*,*) "Sum is:", maximum
-				write (*,"(A,I3,A,I3,A,I3,A,I3,A)") "(",r1, ",",c1, ") (",r2, ",",c2,")"
-			end if
-			call mpi_finalize(mpi_err)
-		end subroutine
-end module kadane_mpi
+      N = size(A, 1); M = size(A, 2)
+      allocate(p(M))
+
+      mpi_master = 0
+      call mpi_comm_rank(mpi_comm_world, mpi_rank, mpi_err)
+      call mpi_comm_size(mpi_comm_world, mpi_size, mpi_err)
+      allocate(maximums(mpi_size))
+
+      maximum = 0; x1  = 0; y1  = 0; x2  = 0; y2  = 0
+      do g = 1, N
+         if (mod(g, mpi_size) == mpi_rank) then
+            do j = 1, M
+               p(j) = 0
+            end do
+            do i = g, N
+               t = 0; h = 1
+               do j = 1 , M
+                  p(j) = p(j) + A(i,j); t = t+p(j)
+                  if (t>maximum) then
+                     maximum = t; x1 = g; y1 = h; x2 = i; y2 = j
+                  else if (t<=0) then
+                     t = 0; h = j + 1
+                  end if
+               end do
+            end do
+         end if
+      end do
+      call mpi_allgather(maximum, 1, mpi_real8, maximums, 1, mpi_real8, mpi_comm_world, mpi_err)
+      mpi_correct = maxloc(maximums, 1) - 1
+      call mpi_bcast(x1, 1, mpi_real8, mpi_correct, mpi_comm_world, mpi_err)
+      call mpi_bcast(y1, 1, mpi_real8, mpi_correct, mpi_comm_world, mpi_err)
+      call mpi_bcast(x2, 1, mpi_real8, mpi_correct, mpi_comm_world, mpi_err)
+      call mpi_bcast(y2, 1, mpi_real8, mpi_correct, mpi_comm_world, mpi_err)
+    end subroutine
+end module
--- main_kadane_mpi.f90
+++ main_kadane_mpi.f90
@@ -1,5 +1,38 @@
 program main_kadane_mpi
-  use kadane_mpi
-  
-  call kadane
+  use Homework_MPI
+  use mpi
+  real(8), allocatable :: A (:,:)
+  integer(4) :: x1, y1, x2, y2, i ,j, N, M
+  real(8) :: r
+  integer(4) :: mpi_master, mpi_rank, mpi_err
+
+  call mpi_init(mpi_err)
+  mpi_master = 0
+  call mpi_comm_rank(mpi_comm_world, mpi_rank, mpi_err)
+
+  if (mpi_rank == mpi_master) then
+     write(*,*) "N, M = ?" 
+     read(*,*) N, M
+  end if
+  call mpi_bcast(N, 1, mpi_integer, mpi_master, mpi_comm_world, mpi_err)
+  call mpi_bcast(M, 1, mpi_integer, mpi_master, mpi_comm_world, mpi_err)
+  allocate(A(N,M))
+  if (mpi_rank == mpi_master) then
+     call random_seed
+     do i = 1, N
+        do j = 1, M
+           call random_number(r)
+           A(i,j) = r * 100-50 
+           write (*,"(F5.1,A)", advance="no") A(i,j), " "
+        end do
+        write (*,*)
+     end do
+  end if
+  call mpi_bcast(A, N*M, mpi_real8, mpi_master, mpi_comm_world, mpi_err)
+
+  call FindMaxCoordinates(A, x1, y1, x2, y2)
+  if(mpi_rank == mpi_master) then
+     write (*,"(A,I3,A,I3,A,I3,A,I3,A)") "(",x1, ",",y1, ") (",x2, ",",y2,")"
+  end if
+  call mpi_finalize(mpi_err)
 end program
